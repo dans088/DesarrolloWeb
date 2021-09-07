0db82e9a1d60f135b667c020a9620c2917b0f579
dans088 <daniel.atilano@live.com>
2021-09-06 23:13:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-//librerias ó components
 import "./App.css";
 import "./index.css";
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import {Home, About, Dashboard} from "./components/NewInput"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}from "react-router-dom";
 
 function App() {
   
-  const [users, setUsers] = useState([{ id: 0, name: "Ruben" }]);
+  const [date, setDate] = useState(new Date());
+  const [seconds, setSeconds] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+  const [horas, setHoras] = useState(0);
 
-  const onClick = () => {};  
+  
+  useEffect(() => {
+    let interval = null;
+    
+      interval = setInterval(() => {
+        if (seconds > 59){
+          setMinutos(minutos => minutos + 1);  
+          setSeconds(0);
+        }
+        else if (minutos>59){
+          setHoras(horas => horas + 1);  
+          setMinutos(0);  
+          setSeconds(0);
 
+        }
+        else{
+        setSeconds(seconds => seconds + 1);
+        }
+      }, 100);
+    
+    return () => clearInterval(interval);
+  });
+
+  
   return (
     <div>
-      <input type="button" onClick={onClick} value="Update" />
-      <div>
-        {users.map((e) => (
-          <div>
-            {e.id} - {e.name}
-          </div>
-        ))}
-      </div>
+      {horas} - {minutos} - {seconds}
     </div>
   );
 }
 
-export default App;
+
+export default function BasicExample()
+{
+  return(
+    <Router>
+      <Link style={{padding: "30px"}} to= "/">Home</Link>
+
+      <Link style={{padding: "30px"}} to= "/about">About</Link>
+
+      <Link style={{padding: "30px"}} to= "/dashboard">Dashboard</Link>
+    
+    <hr />
+
+    <Switch>
+      <Route exact path = "/">
+        <Home />
+      </Route>
+      <Route exact path = "/about">
+        <About />
+      </Route>
+      <Route exact path = "/dashboard">
+        <Dashboard />
+      </Route>
+
+    </Switch>
+
+
+    </Router>
+
+  );
+};
